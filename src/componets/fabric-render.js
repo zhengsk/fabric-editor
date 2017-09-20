fd10b877afaa3143b8cd42bf813fab7efb3a5b99
zsk <zsk526@qq.com>
2017-09-21 00:36:36 +0800

    Add fabric-render component

---
+++ src/componets/fabric-render.js
@@ -0,0 +1,60 @@
+import transformImage from 'transform-image';
+
+export default {
+    name: 'fabric-render',
+
+    data: function() {
+        return {
+            "version": 1,
+            "plates": [{
+                "plate": "plate url", // 鞋面图片
+                "template": "{json data}", // editor image
+                "url": "resultImageUrl"
+            }],
+
+            "rendering": [{
+                "picture": "picture url", // 鞋子照片
+                "mask": [{
+                    "plate": 3,
+                    "url": "mask url",
+                    "points": [
+                        [33, 44],
+                        [500, 94],
+                        [33, 800],
+                        [799, 600]
+                    ]
+                }]
+            }],
+        };
+    },
+
+    methods: {
+        // 获取变形后的图片
+        getTransformImage(imageSrc, maskSrc, points) {
+            return transformImage({
+                imageSrc: imageSrc,
+                maskSrc: maskSrc,
+                points: points, // 默认位置
+            });
+        },
+
+        renderShose() {
+            this.rendering.forEach(shoes => {
+                const maskedImage = shoes.mask.forEach(mask => {
+                    return this.getTransformImage(
+                        this.plates[mask.plate],
+                        mask.url,
+                        mask.points
+                    );
+                });
+
+            });
+        }
+    },
+
+    mounted() {
+
+    }
+
+
+}
